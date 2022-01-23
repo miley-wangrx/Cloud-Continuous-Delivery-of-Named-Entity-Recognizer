f80889a83e92194cc32834b8da44001f422b7661
miley-wangrx <miley.wangrx@foxmail.com>
2022-01-23 23:37:44 +0000

    m

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello Duke"}
 
-@app.get("/add/{num1}/{num2}") #route, where parameters get passed in
-async def add(num1: int, num2: int): # function
+
+@app.get("/add/{num1}/{num2}")  # route, where parameters get passed in
+async def add(num1: int, num2: int):  # function
     """Add two numbers together"""
 
     total = num1 + num2
     return {"total": total}
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
