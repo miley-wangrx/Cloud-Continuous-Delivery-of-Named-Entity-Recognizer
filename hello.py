13681ab7732564cdb0038fa8fad2e8e23ec64970
miley-wangrx <miley.wangrx@foxmail.com>
2022-01-23 23:52:08 +0000

    modified testing

--- hello.py
+++ hello.py
@@ -3,4 +3,15 @@ def cool(name):
         return "Cool!"
     return "Not cool at all.."
 
+
 print(cool("Miley"))
+
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+    
--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,23 @@
-from hello import cool
+from hello import cool, toyou, add, subtract
 
 
 def test_cool():
     assert cool("Miley") == "Cool!"
     assert cool("Bubble") == "Not cool at all.."
+
+def setup_function(function):
+    print(f" Running Setup: {function.__name__}")
+    function.x = 10
+
+
+def teardown_function(function):
+    print(f" Running Teardown: {function.__name__}")
+    del function.x
+
+
+### Run to see failed test
+#def test_hello_add():
+#    assert add(test_hello_add.x) == 12
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
