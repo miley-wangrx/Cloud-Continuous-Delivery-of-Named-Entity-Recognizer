24bc8a38ca04a7da53c2d47e97d3223ec8c2eeb3
miley-wangrx <miley.wangrx@foxmail.com>
2022-01-30 05:20:16 +0000

    finished parser

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+import spacy
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Duke"}
+    return {"message": "Welcome to Ruoxuan's NLP parser"}
 
 
 @app.get("/add/{num1}/{num2}")  # route, where parameters get passed in
@@ -16,6 +17,21 @@ async def add(num1: int, num2: int):  # function
     total = num1 + num2
     return {"total": total}
 
+@app.get("/<input_sentence>") # route, where parameters get passed in
+async def parser(input_sentence):
+    model = spacy.load("en_core_web_sm")
+    output = {}
+    doc = model(input_sentence)
+    for entity in doc.ents:
+        if entity.label_ == "GPE":
+            label = "COUNTRY"
+        elif entity.label_ == "LOC":
+            label = "LOCATION"
+        else:
+            label = entity.label_
+        output[entity.text] = label
+
+    return output
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
