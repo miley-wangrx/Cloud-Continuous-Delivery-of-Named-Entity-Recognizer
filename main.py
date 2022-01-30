03df79f1f4bad2b93330a78bc965c1fcef48d5d3
miley-wangrx <miley.wangrx@foxmail.com>
2022-01-30 05:55:36 +0000

    finished NLP function

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ async def add(num1: int, num2: int):  # function
     total = num1 + num2
     return {"total": total}
 
-# @app.get("/<input_sentence>") # route, where parameters get passed in
-# async def parser(input_sentence):
-#     model = spacy.load("en_core_web_sm")
-#     output = {}
-#     doc = model(input_sentence)
-#     for entity in doc.ents:
-#         if entity.label_ == "GPE":
-#             label = "COUNTRY"
-#         elif entity.label_ == "LOC":
-#             label = "LOCATION"
-#         else:
-#             label = entity.label_
-#         output[entity.text] = label
+@app.get("/<input_sentence>") # route, where parameters get passed in
+async def parser(input_sentence):
+    model = spacy.load("en_core_web_sm")
+    output = {}
+    doc = model(input_sentence)
+    for entity in doc.ents:
+        if entity.label_ == "GPE":
+            label = "COUNTRY"
+        elif entity.label_ == "LOC":
+            label = "LOCATION"
+        else:
+            label = entity.label_
+        output[entity.text] = label
 
-#     return output
+    return output
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
