2004ab36172a6560b155ac8b62efd050ceb7dff9
miley-wangrx <miley.wangrx@foxmail.com>
2022-01-30 06:11:07 +0000

    finished debugging new function

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ async def add(num1: int, num2: int):  # function
     total = num1 + num2
     return {"total": total}
 
-@app.get("/<input_sentence>") # route, where parameters get passed in
-async def parser(input_sentence):
+@app.get("/predict/{input_sentence}") # route, where parameters get passed in
+async def predict(input_sentence):
     model = spacy.load("en_core_web_sm")
     output = {}
     doc = model(input_sentence)
